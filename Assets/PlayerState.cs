6ee812404ffcdede5a2eee11eda1c7ea362e0ebd
20181181송영훈 <83389557+songyeonghun@users.noreply.github.com>
2023-03-10 00:17:09 +0900

    스탯표 제작

--- Assets/PlayerState.cs
+++ Assets/PlayerState.cs
@@ -8,13 +8,23 @@ public class PlayerState : MonoBehaviour
     static public int statAtk;
     static public int statMoveSpeed;
     static public int statAtkSpeed;
-    static public int StatHpRegen;
     static public int statHpUse;
+    static public float StatHpRegen;
 
+    //스탯표
+    public int[][] stat = new int[6][]
+    {
+        new int[]{ 300,320,340,360,380,400,420,440,460,480,500},    //0 채력
+        new int[]{ 100,110,120,130,140,150},                            //1 공격력
+        new int[]{ 10,11,12},                                              //2 이동속도
+        new int[]{ 10,11,12,13,14,15},                                    //3 공격속도
+        new int[]{ 3,2},                                                    //4 채력소모
+        new int[]{ 30,40,50}                                               //5 채력회복
+    };
 
     void Start()
     {
-        statHp = PlayerPrefs.GetInt("StatHP");
+        statHp = PlayerPrefs.GetInt("statHP");
         statAtk = PlayerPrefs.GetInt("statAtk");
         statMoveSpeed = PlayerPrefs.GetInt("statMoveSpeed");
         statAtkSpeed = PlayerPrefs.GetInt("statAtkSpeed");
@@ -22,8 +32,35 @@ public class PlayerState : MonoBehaviour
         statHpUse = PlayerPrefs.GetInt("statHpUse");
     }
 
-    void Update()
+    public void HpUp()
     {
-        
+        if (statHp < 10)
+        statHp++;
+        Debug.Log(statHp);
+    }
+    public void AtkUp()
+    {
+        if (statAtk < 5)
+            statAtk++;
+    }
+    public void MoveSpeedUp()
+    {
+        if(statMoveSpeed<3)
+        statMoveSpeed++;
+    }
+    public void AtkSpeedUp()
+    {
+        if(statAtkSpeed<5)
+        statAtkSpeed++;
+    }
+    public void HpRegenUp()
+    {
+        if(StatHpRegen<2)
+        StatHpRegen++;
+    }
+    public void HpUseUp()
+    {
+        if(statHpUse<3)
+        statHpUse++;
     }
 }
